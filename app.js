329507a5cc8d5914ad1eab057b252cb428f28730
MIchael Huang <michaelnctu@gmail.com>
2020-09-06 11:51:31 +0800

    detail feature done

--- app.js
+++ app.js
@@ -29,6 +29,13 @@ app.get('/users/login', (req, res) => {
   res.render('login')
 })
 
+app.get('/todos/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
 app.post('/users/login', (req, res) => {
   res.send('login')
 })
