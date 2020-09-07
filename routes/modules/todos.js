50d55098f7e8a293a0da8f3ea4128f3d2ab1192b
MIchael Huang <michaelnctu@gmail.com>
2020-09-07 14:16:09 +0800

    edit feature

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -4,14 +4,6 @@ const router = express.Router()
 const db = require('../../models')
 const Todo = db.Todo
 
-
-// router.get('/:id', (req, res) => {
-//   const id = req.params.id
-//   return Todo.findByPk(id)
-//     .then(todo => res.render('detail', { todo: todo.toJSON() }))
-//     .catch(error => console.log(error))
-// })
-
 router.get('/new', (req, res) => {
   return res.render('new')
 })
@@ -20,6 +12,40 @@ router.post('/', (req, res) => {
   const name = req.body.name       // 從 req.body 拿出表單裡的 name 資料
   return Todo.create({ name })     // 存入資料庫
     .then(() => res.redirect('/')) // 新增完成後導回首頁
+    .catch(error => console.log(error))
+})
+
+//detail樣板
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  return Todo.findByPk(id)  //mongodb findById
+    .then(todo => res.render('detail', { todo: todo.toJSON() }))
+    .catch(error => console.log(error))
+})
+
+//detail樣板
+router.get('/:id/edit', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => res.render('edit', { todo: todo.get() }))
+    .catch(error => console.log(error))
+})
+
+router.put('/:id', (req, res) => {
+  const UserId = req.user.id
+  const id = req.params.id
+  const { name, isDone } = req.body
+  console.log(req.body)
+
+  return Todo.findOne({ where: { id, UserId } })
+    .then(todo => {
+      todo.name = name
+      todo.isDone = isDone === 'on'
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
     .catch(error => console.log(error))
 })
 
