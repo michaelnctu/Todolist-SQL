93cf350edba6285e235e0623989c465a5c4e23e0
MIchael Huang <michaelnctu@gmail.com>
2020-09-07 16:34:01 +0800

    authenticate middleware feature

--- routes/index.js
+++ routes/index.js
@@ -4,11 +4,10 @@ const router = express.Router()
 
 const home = require('./modules/home')
 const todos = require('./modules/todos')
-const users = require('./modules/users') /
+const users = require('./modules/users')
 
 
-
-  router.use('/', home)
+router.use('/', home)
 router.use('/todos', todos)
 router.use('/users', users)
 
